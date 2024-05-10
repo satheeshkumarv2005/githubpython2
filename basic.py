a4618a83b20b36cfc326edd12ec9788d6509d230
satheeshkumarv2005 <satheeshkumarv2005@gmai.com>
2024-05-10 19:32:55 +0530

    basic program

---
+++ basic.py
@@ -0,0 +1,6 @@
+#1.Using print(),write a program yourself?
+print("my name is satheesh","my age is 25","i am learning python")                  #1st method print()
+print("my name is satheesh","my age is 25","i am learning python",sep = "\n")       #2nd method print()
+print("my name is satheesh",end = " ")                                              #3rd method print()         
+print("my age is 25",end = " ")
+print("i am learning python",end = " ")
